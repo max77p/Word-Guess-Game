7d35a640f7466c9ed1db18708b43c9757978f236
max77p <shanindrakumar@gmail.com>
2018-04-26 16:28:39 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,207 @@
+/*$(".gameMessages").prepend("Welcome to the schoolyard Hangman");
+var keysPressedArr = [];
+var regex = new RegExp(/[a-zA-Z.]/);
+var displayMan = 0;
+
+$('.whenClicked').on("click", function () {
+    hangmanGame['listenforkey'];
+    hangmanGame['startGame'];
+    gameOver = false;
+});
+
+var hangmanGame = {
+
+    startGame: function () {
+        keysPressedArr = [];
+        displayMan = 0;
+        randomword = 0;
+        score = 7;
+        hangmanarr = [];
+        convert = 0;
+        tempArr = [];
+        $(".hideMan").hide();
+        $("#placehere").empty();
+        $(".showMan").hide();
+        $("#alphabet").empty();
+        startMessage();
+        var words = new Array("Toronto", "yosemite", "travel", "wonderland", "dog", "business", "candy");
+        randomword = words[Math.floor(Math.random() * words.length)];
+        hangmanarr = randomword.toUpperCase().split("");
+        //console.log(randomword);
+
+        console.log(hangmanarr);
+
+        //add the blanks to html
+        for (var i = 0; i < hangmanarr.length; i++) {
+            var x = document.getElementById('placehere');
+            var spacing = "&nbsp;&nbsp;";
+            x.innerHTML += '<span class="blankSpace">' + "__" + spacing + '</span>' + "  ";
+        }
+
+    },
+
+    listenforkey: function () {
+        document.addEventListener('keyup', function (event) {
+            if (gameOver) {
+                return;
+            }
+
+            convert = String.fromCharCode(event.keyCode);
+            if (hangmanarr.indexOf(convert) < 0 && regex.test(convert) === true && keysPressedArr.indexOf(convert) < 0) {
+                lettersPressed(convert);
+                score--;
+                console.log(score);
+                startMessage();
+                showBody();
+            }
+            else if (hangmanarr.indexOf(convert) >= 0 && regex.test(convert) === true); {
+                insertLetter(convert);
+                lettersPressed(convert);
+            }
+            if (score < 1) {
+                gameOverMsg();
+            }
+        });
+
+    },
+
+    lettersPressed: function () {
+        var y = document.getElementById('alphabet');
+        var spacing = "&nbsp;&nbsp;";
+        if (keysPressedArr.indexOf(x) < 0 && regex.test(convert) === true) {
+            keysPressedArr.push(x);
+            var spacing = "&nbsp;&nbsp;";
+            y.innerHTML += x + spacing;
+        }
+        else {
+            return;
+        }
+    },
+
+    gameOverMsg: function () {
+        $(".gameMessages").empty().prepend("Game Over! To play again press Start");
+        restartGame();
+    },
+
+    startMessage: function () {
+        $(".gameMessages").empty().prepend("You have" + " " + score + " " + "guesses");
+    },
+
+    restartGame: function () {
+        winsCounter = 0;
+        $("#winNumber").empty();
+    },
+    showGameWin: function () {
+        var guessedWord = tempKey.join("");
+        var mainWord = hangmanarr.join("");
+        var mainIdx = hangmanarr.length;
+        if (guessedWord === mainWord) {
+            winsCounter();
+            gameOver = true;
+        }
+
+    },
+
+    winsCounter: function () {
+        $(".gameMessages").empty().prepend("You have won!");
+        winCount++;
+        $("#winNumber").html(winCount);
+    },
 
 
+    sendRepeatMessage: function () {
+        $(".gameMessages").empty().prepend("You have pressed and invalid key or repeated a letter");
+        setTimeout(function () {
+            $(".gameMessages").empty().prepend("You have" + " " + score + " " + "guesses");
+        }, 2000);
+    },
+    showBody: function () {
+        var k = document.getElementsByClassName('showMan');
+        console.log(k);
+        if (k.length > displayMan) {
+            k[displayMan].style.display = "block";
+            displayMan++;
+        }
+    },
 
-    $(".gameMessages").prepend("Welcome to the schoolyard Hangman");
+    insertLetter: function () {
+        var element = document.getElementsByClassName('blankSpace'); //get array of the html span
+        var findInArray = [];
+        for (var i = 0; i < hangmanarr.length; i++) {
+
+            if (hangmanarr[i] === y) {
+                findInArray.push(i);//push position of letter to this array
+                tempArr[i] = y;
+            }
+        }
+        for (var k = 0; k < findInArray.length; k++) {
+
+            element[findInArray[k]].innerHTML = y;//adding matched letter to inner html of span according to position
+        }
+
+        showGameWin(tempArr, y);
+    }
 
 
+};
+*/
 
+
+$(".gameMessages").prepend("Welcome to the schoolyard Hangman");
+
+
+    
 
 var tempArr = [];
-var score=7;
+var score = 7;
 var randomword;
 var hangmanarr;
 var winCount = 0;
 var convert;
 var gameOver;
+var veryFirstGame;
 //console.log(words);
 
+function hint(el){
+$(".hintBtn").on('click',function(){
+    $(".hintBtn").empty().prepend(el);
+    setTimeout(function () {
+        $(".hintBtn").empty().prepend("Hint?");
+    }, 2000);
+});
+}
 
-$('.whenClicked').on("click", function () {  
+$('.whenClicked').on("click", function () {
     listenforkey();
     startGame();
-    gameOver=false;
+    gameOver = false;
 });
 
+var words = {
+    City:["toronto","california","london","montreal"],
+    Sports:["soccer","curling","jordan","stadium","rink"],
+    PopCulture:["katy","movie","jackson","instagram","hollywood"],
+    Animals:["dog","hippo","rhino","panda"]
+}
+
+var pickcategory=function(){
+    var keywords=Object.keys(words);
+    var combine="";
+    var selection;
+    //console.log(keywords);
+    var getrandomcategory= keywords[Math.floor(Math.random() * keywords.length)];
+    hint(getrandomcategory);
+    var wordsforCategory=words[getrandomcategory];
+    temprandomword = wordsforCategory[Math.floor(Math.random() * wordsforCategory.length)]
+    var temphangmanarr = temprandomword.toUpperCase().split("")
+    return temphangmanarr;
+}
 
 
+
+  
+  
+  
 
 //fresh game start
 function startGame() {
@@ -32,16 +210,17 @@ function startGame() {
     randomword = 0;
     score = 7;
     hangmanarr = [];
-    convert=0;
-    tempArr=[];
+    convert = 0;
+    tempArr = [];
     $(".hideMan").hide();
     $("#placehere").empty();
     $(".showMan").hide();
     $("#alphabet").empty();
     startMessage();
-    var words = new Array("Toronto", "yosemite", "travel", "wonderland", "dog", "business", "candy");
-    randomword = words[Math.floor(Math.random() * words.length)];
-    hangmanarr = randomword.toUpperCase().split("");
+    hangmanarr=pickcategory();
+    //var words = new Array("Toronto", "yosemite", "travel", "wonderland", "dog", "business", "candy");
+    //randomword = words[Math.floor(Math.random() * words.length)];
+    //hangmanarr = randomword.toUpperCase().split("");
     //console.log(randomword);
 
     console.log(hangmanarr);
@@ -63,81 +242,73 @@ function listenforkey(element) {
 
     document.addEventListener('keyup', function (event) {
         //console.log(event.keyCode);
-        
-        if(gameOver){
+
+        if (gameOver) {
             return;
         }
-      
-      convert = String.fromCharCode(event.keyCode);
-        if (hangmanarr.indexOf(convert) < 0 && regex.test(convert) === true && keysPressedArr.indexOf(convert)<0) {          
-            lettersPressed(convert);          
-            score--; 
+
+        convert = String.fromCharCode(event.keyCode);
+        if (hangmanarr.indexOf(convert) < 0 && regex.test(convert) === true && keysPressedArr.indexOf(convert) < 0) {
+            lettersPressed(convert);
+            score--;
             console.log(score);
-            startMessage();     
-               showBody();
+            startMessage();
+            showBody();
         }
         else if (hangmanarr.indexOf(convert) >= 0 && regex.test(convert) === true); {
-            insertLetter(convert);          
+            insertLetter(convert);
             lettersPressed(convert);
         }
-        if(score<1){
+        if (score < 1) {
             gameOverMsg();
         }
-  
+
 
 
     });
 }
 
 
-//lettersGuessed --push all non repeating letters to html
-/*function lettersGuessed(lg) {
-
-    var y = document.getElementById('alphabet');
-    //console.log(lg);
-    var spacing = "&nbsp;&nbsp;";
-    y.innerHTML += lg + spacing;
-
-}*/
-
-
 
 //add all letters pressed on keyboard in array, but not including repeating letters
-
 var keysPressedArr = [];
 function lettersPressed(x) {
-   //console.log(keysPressedArr);
-   var y = document.getElementById('alphabet');
-   var spacing = "&nbsp;&nbsp;";
-   if(keysPressedArr.indexOf(x)<0 && regex.test(convert) === true){
+    var y = document.getElementById('alphabet');
+    var spacing = "&nbsp;&nbsp;";
+    if (keysPressedArr.indexOf(x) < 0 && regex.test(convert) === true) {
         keysPressedArr.push(x);
         var spacing = "&nbsp;&nbsp;";
         y.innerHTML += x + spacing;
     }
-    else{
+    else {
         return;
     }
-   
+
 }
 
 
 //keepscore
-function gameOverMsg() {  
-    $(".gameMessages").empty().prepend("Game Over!");   
+function gameOverMsg() {
+    $(".gameMessages").empty().prepend("Game Over! To play again press Start");
+    winCount=0;
+    $("#winNumber").empty();
+    gameOver = true;
+
+}
+
+function restartGame() {
+    winsCounter = 0;
+    $("#winNumber").empty();
 }
 
 //show end of game screen
 function showGameWin(tempKey, pressedkey) {
-    //var checkarray=tempKey.join("").split("");
     var guessedWord = tempKey.join("");
     var mainWord = hangmanarr.join("");
-    //console.log(mainWord);
-
     var mainIdx = hangmanarr.length;
-    //console.log(mainIdx);
     if (guessedWord === mainWord) {
-        winsCounter(); 
-        gameOver=true; 
+        winsCounter();
+        gameOver = true;
     }
 
 }
@@ -174,9 +345,9 @@ var displayMan = 0;
 function showBody() {
     var k = document.getElementsByClassName('showMan');
     console.log(k);
-    if(k.length>displayMan){
+    if (k.length > displayMan) {
         k[displayMan].style.display = "block";
-        displayMan++;   
+        displayMan++;
     }
 }
 
@@ -187,10 +358,7 @@ function showBody() {
 
 //insert letter into html
 function insertLetter(y) {
-    //console.log(y);
     var element = document.getElementsByClassName('blankSpace'); //get array of the html span
-
-
     var findInArray = [];
     for (var i = 0; i < hangmanarr.length; i++) {
 
@@ -198,23 +366,11 @@ function insertLetter(y) {
             findInArray.push(i);//push position of letter to this array
             tempArr[i] = y;
         }
-
     }
-
-    console.log(tempArr);
-
-
     for (var k = 0; k < findInArray.length; k++) {
 
         element[findInArray[k]].innerHTML = y;//adding matched letter to inner html of span according to position
-
-
-
-
     }
 
     showGameWin(tempArr, y);
-
-
 }
-//console.log(tempArr.join("").toUpperCase().split(""));
